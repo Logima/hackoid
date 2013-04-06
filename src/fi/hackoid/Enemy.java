bdeed40049c4308a2a5b59ff434efc6a76b92881
Logima <logimaru@gmail.com>
2013-04-07 01:43:34 +0300

    Use new player and enemy textures

--- src/fi/hackoid/Enemy.java
+++ src/fi/hackoid/Enemy.java
@@ -1,6 +1,5 @@
 package fi.hackoid;
 
-import org.andengine.engine.handler.physics.PhysicsHandler;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
@@ -20,53 +19,42 @@ public class Enemy {
 	private BitmapTextureAtlas textureAtlas;
 	private TiledTextureRegion textureRegion;
 
-	//private PhysicsHandler physicsHandler;
-
 	private AnimatedSprite animatedSprite;
-	
+
 	Body body;
-	
+
 	private static final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
+
+	private long[] frameTimes = new long[] { 0, 0, 100, 100, 100, 100, 100, 100, 100, 100, 0, 0, 100, 100, 100, 100,
+			100, 100, 100, 100 };
 
 	public Enemy() {
 	}
 
 	public void createResources(Main main) {
-		textureAtlas = new BitmapTextureAtlas(main.getTextureManager(), 4096, 512, TextureOptions.BILINEAR);
+		textureAtlas = new BitmapTextureAtlas(main.getTextureManager(), 2048, 1024, TextureOptions.BILINEAR);
 		textureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(textureAtlas, main,
-				"monster_teekkari_walking_left.png", 0, 0, 11, 2);
+				"monster_teekkari_walking.png", 0, 0, 10, 4);
 		textureAtlas.load();
 	}
 
-	public void createScene(VertexBufferObjectManager vertexBufferObjectManager, int cameraWidth, int cameraHeight, PhysicsWorld world) {
-		/*
-		 * Calculate the coordinates for the face, so its centered on the
-		 * camera.
-		 */
-		//final float playerX = cameraWidth - textureRegion.getWidth();
-		//final float playerY = cameraHeight - textureRegion.getHeight();
-		
+	public void createScene(VertexBufferObjectManager vertexBufferObjectManager, int cameraWidth, int cameraHeight,
+			PhysicsWorld world) {
 		float playerX = 500;
 		float playerY = 200;
-		
+
 		animatedSprite = new AnimatedSprite(playerX, playerY, textureRegion, vertexBufferObjectManager);
 		animatedSprite.setScaleCenterY(textureRegion.getHeight());
 		animatedSprite.setScale(1);
 
-		long[] frameTimes = new long[] { 0, 0, 100, 100, 100, 100, 100, 100, 100, 100, 0, 0, 0, 100, 100, 100, 100,
-				100, 100, 100, 100, 0 };
-		animatedSprite.animate(frameTimes, 0, 21, true);
+		animatedSprite.animate(frameTimes, 0, 19, true);
 
-		
-		
-		
 		animatedSprite.registerUpdateHandler(world);
-		
-		
+
 		body = PhysicsFactory.createBoxBody(world, animatedSprite, BodyType.DynamicBody, FIXTURE_DEF);
-		
+
 		world.registerPhysicsConnector(new PhysicsConnector(animatedSprite, body, true, false));
-		
+
 		body.setLinearVelocity(-10 * 2, 0);
 	}
 
--- src/fi/hackoid/Player.java
+++ src/fi/hackoid/Player.java
@@ -1,6 +1,5 @@
 package fi.hackoid;
 
-import org.andengine.engine.handler.physics.PhysicsHandler;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
@@ -22,18 +21,19 @@ public class Player {
 	private BitmapTextureAtlas textureAtlas;
 	private TiledTextureRegion textureRegion;
 
-	//private PhysicsHandler playerPhysicsHandler;
-
 	private AnimatedSprite animatedSprite;
 
 	private float oldSpeed;
-	
+
 	private boolean facingRight = true;
-	
+
 	private Main context;
-	
+
 	private static final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
-	
+
+	private long[] frameTimes = new long[] { 0, 0, 100, 100, 100, 100, 100, 100, 100, 100, 0, 0, 100, 100, 100, 100,
+			100, 100, 100, 100 };
+
 	Body body;
 
 	public Player() {
@@ -41,17 +41,15 @@ public class Player {
 
 	public void createResources(Main main) {
 		this.context = main;
-		textureAtlas = new BitmapTextureAtlas(main.getTextureManager(), 4096, 512, TextureOptions.BILINEAR);
+		textureAtlas = new BitmapTextureAtlas(main.getTextureManager(), 2048, 1024, TextureOptions.BILINEAR);
 		textureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(textureAtlas, main,
-				"player_walking_right.png", 0, 0, 11, 2);
+				"player_walking.png", 0, 0, 10, 4);
 		textureAtlas.load();
 	}
 
-	public void createScene(VertexBufferObjectManager vertexBufferObjectManager, int cameraWidth, int cameraHeight, PhysicsWorld world) {
-		//final float playerX = (cameraWidth - textureRegion.getWidth()) / 2;
-		//final float playerY = cameraHeight - textureRegion.getHeight() - 5;
-		
-		final float playerX  = 100;
+	public void createScene(VertexBufferObjectManager vertexBufferObjectManager, int cameraWidth, int cameraHeight,
+			PhysicsWorld world) {
+		final float playerX = 100;
 		final float playerY = 200;
 
 		animatedSprite = new AnimatedSprite(playerX, playerY, textureRegion, vertexBufferObjectManager);
@@ -60,42 +58,36 @@ public class Player {
 
 		animatedSprite.setCurrentTileIndex(0);
 
-		//playerPhysicsHandler = new PhysicsHandler(animatedSprite);
 		animatedSprite.registerUpdateHandler(world);
-		
-		
+
 		body = PhysicsFactory.createBoxBody(world, animatedSprite, BodyType.DynamicBody, FIXTURE_DEF);
-		
-		
+
 		world.registerPhysicsConnector(new PhysicsConnector(animatedSprite, body, true, false));
 	}
 
 	public void run(float speed) {
 		body.setLinearVelocity(speed / 20, body.getLinearVelocity().y);
-		
 
 		if (speed == 0) {
 			animatedSprite.stopAnimation();
 			animatedSprite.setCurrentTileIndex(0);
 		} else if (oldSpeed == 0) {
-			long[] frameTimes = new long[] { 0, 0, 100, 100, 100, 100, 100, 100, 100, 100, 0, 0, 0, 100, 100, 100, 100,
-					100, 100, 100, 100, 0 };
-			animatedSprite.animate(frameTimes, 0, 21, true);			
-		}
-		
-		if (this.facingRight && speed < 0 || !this.facingRight && speed > 0) {
+			animateRun();
+		} else if (this.facingRight && speed < 0 || !this.facingRight && speed > 0) {
 			this.facingRight = !this.facingRight;
-			toggleDirection();
+			animateRun();
 		}
-		
+
 		oldSpeed = speed;
 	}
-	
-	private void toggleDirection() {
-		this.textureAtlas.clearTextureAtlasSources();
-		BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(textureAtlas, this.context,
-				this.facingRight ? "player_walking_right.png" : "player_walking_left.png", 0, 0, 11, 2);
-}
+
+	private void animateRun() {
+		if (facingRight) {
+			animatedSprite.animate(frameTimes, 0, 19, true);
+		} else {
+			animatedSprite.animate(frameTimes, 20, 39, true);
+		}
+	}
 
 	public Body getPhysicsBody() {
 		return body;
