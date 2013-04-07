364ed308e99ad61b4cc71ab1959060db65fb6cc6
Aleksi J Majander <ajmajand@e42-20.cs.helsinki.fi>
2013-04-07 14:31:50 +0300

    Added title and story screens

---
+++ src/fi/hackoid/TitleActivity.java
@@ -0,0 +1,38 @@
+package fi.hackoid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class TitleActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		//Remove notification bar
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_title);
+	}
+	
+	public void start(View view) {
+		Intent intent = new Intent(this, StoryActivity.class);
+		startActivity(intent);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.title, menu);
+		return true;
+	}
+
+}
